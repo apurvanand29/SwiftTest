c631bc7d0df00b03c84bf3082b8043ec8ffad606
Apurva Anand <apurvanand29@gmail.com>
2025-03-01 12:21:39 -0600

    TestRunner Setup and dependencies

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,4 +1,15 @@
 package runner;
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = {"src/test/java/StepDefinition", "src/test/java/utility"},
+        plugin = {"pretty", "html:target/cucumber-html-report", "json:cucumber.json"}
+)
 
 public class TestRunner {
+
 }
