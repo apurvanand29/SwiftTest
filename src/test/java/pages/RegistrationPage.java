55cabd5cc606b715ebdb6e32dddc89e7912a415d
Apurva Anand <apurvanand29@gmail.com>
2025-03-02 10:20:03 -0600

    Added wait:registration Page and updated beforeAll:one browser session

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -4,12 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utility.BrowserDriver;
+
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
 
 public class RegistrationPage extends BrowserDriver {
     WebDriver driver;
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
     @FindBy(xpath = "//h1[text()='User Registration Page']")
     public WebElement userRegistrationPage;
@@ -22,6 +27,7 @@ public class RegistrationPage extends BrowserDriver {
 
     // User verifies current page is Registration page
     public void checkUserRegistrationPage(){
+        wait.until(ExpectedConditions.visibilityOf(userRegistrationPage));
         String actualTitle = userRegistrationPage.getText();
         assertEquals("User Registration Page", actualTitle);
     }
--- src/test/java/utility/Hooks.java
+++ src/test/java/utility/Hooks.java
@@ -3,22 +3,24 @@ package utility;
 
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hooks extends BrowserDriver{
 
-
-    @Before
-    public void setUp(){
+    // To ensure 1 browser session
+    @BeforeAll
+    public static void setUp(){
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver");
         BrowserDriver.driver = new ChromeDriver();
         BrowserDriver.driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
     }
 
 
-    @After
-    public void tearDown(){
+    @AfterAll
+    public static void tearDown(){
         if (BrowserDriver.driver != null) {
             BrowserDriver.driver.quit(); // Close and quit the driver after tests
         }
