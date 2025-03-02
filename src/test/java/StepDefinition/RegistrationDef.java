a80d339226f576912249fe0d869cc90fb5f77964
Apurva Anand <apurvanand29@gmail.com>
2025-03-02 11:03:18 -0600

    Added data filling step

--- src/test/java/StepDefinition/RegistrationDef.java
+++ src/test/java/StepDefinition/RegistrationDef.java
@@ -1,14 +1,42 @@
 package StepDefinition;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import pages.RegistrationPage;
 import utility.BrowserDriver;
+import io.cucumber.datatable.DataTable;
+
+import java.util.List;
+import java.util.Map;
 
 public class RegistrationDef {
 
     WebDriver driver = BrowserDriver.driver;
     RegistrationPage registrationPage = new RegistrationPage(driver);
+
+    @When("User enters following on Registration page")
+    public void user_Enters_Following_On_Registration_Page(DataTable dataTable) {
+        // Convert DataTable to a List of Maps (Key-Value Pairs)
+        List<Map<String, String>> registrationData = dataTable.asMaps(String.class, String.class);
+        for (Map<String, String> data : registrationData) {
+            registrationPage.enterRegistrationDetails(
+                    data.get("Salutation"),
+                    data.get("First Name"),
+                    data.get("Last Name"),
+                    data.get("Email id"),
+                    data.get("Contact"),
+                    data.get("Username"),
+                    data.get("Password")
+            );
+        }
+    }
+
+    @And("User hits Submit button")
+    public void user_hits_submit_button() {
+        registrationPage.clickSubmitBtn();
+    }
 
     @Then("User must see User Registration Page")
     public void user_must_see_user_registration_page() {
--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utility.BrowserDriver;
 
@@ -17,7 +18,31 @@ public class RegistrationPage extends BrowserDriver {
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
     @FindBy(xpath = "//h1[text()='User Registration Page']")
-    public WebElement userRegistrationPage;
+    private WebElement userRegistrationPage;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    private WebElement submitBtn;
+
+    @FindBy(xpath = "//select/option")
+    private WebElement salutationDropdown;
+
+    @FindBy(xpath = "//label[@for='firstname']//following::input[@id='firstname']")
+    private WebElement firstNameVal;
+
+    @FindBy(xpath = "//label[@for='lastname']//following::input[@id='lastname']")
+    private WebElement lastNameVal;
+
+    @FindBy(xpath = "//label[@for='emailId']//following::input[@id='emailId']")
+    private WebElement emailVal;
+
+    @FindBy(xpath = "//label[@for='contactNumber']//following::input[@id='contactNumber']")
+    private WebElement contactNumberVal;
+
+    @FindBy(xpath = "//input[@id='usr']")
+    private WebElement usernameVal;
+
+    @FindBy(xpath = "//input[@id='pwd']")
+    private WebElement passwordVal;
 
     // Constructor to initialize elements
     public RegistrationPage(WebDriver driver) {
@@ -30,6 +55,28 @@ public class RegistrationPage extends BrowserDriver {
         wait.until(ExpectedConditions.visibilityOf(userRegistrationPage));
         String actualTitle = userRegistrationPage.getText();
         assertEquals("User Registration Page", actualTitle);
+    }
+
+    //User enters registration data
+    public void enterRegistrationDetails(String salutation, String firstName, String lastName, String emailId, String contactNumber, String username, String password){
+        // Select the Salutation from the dropdown
+        Select salutationSelect = new Select(salutationDropdown);
+        salutationSelect.selectByVisibleText(salutation);
+        firstNameVal.sendKeys(firstName);
+        lastNameVal.sendKeys(lastName);
+        emailVal.sendKeys(emailId);
+        contactNumberVal.sendKeys(contactNumber);
+        usernameVal.sendKeys(username);
+        passwordVal.sendKeys(password);
+    }
+
+    // User hits submit button on registration page
+    public void clickSubmitBtn() {
+        try{
+            submitBtn.click();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
